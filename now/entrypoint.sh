d0e5fa37c816c13f0236eb71ad2e4c47fec3cf2a
Jonathan Gautheron <gautheronjonathan@gmail.com>
2019-09-18 17:56:55 +0200

    Use latest now --prod

--- now/entrypoint.sh
+++ now/entrypoint.sh
@@ -6,7 +6,10 @@ REPO="$(cut -d'/' -f2 <<<"$GITHUB_REPOSITORY")"
 ALIAS="$3.$1"
 DEPLOYMENT="$REPO-$2-$3"
 
+echo $ALIAS
+echo $DEPLOYMENT
+
 mv now.json now-${DEPLOYMENT}.json
 jq --arg alias "$ALIAS" --arg name "$DEPLOYMENT" '.name = $name | .alias = $alias' now-$DEPLOYMENT.json > now.json
 now -t $ZEIT_TOKEN
-now -t $ZEIT_TOKEN alias $ALIAS
+now -t $ZEIT_TOKEN --prod
