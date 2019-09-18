0d247cc25b5b3736db639702bcdbaabc4aaf1160
Jonathan Gautheron <gautheronjonathan@gmail.com>
2019-09-18 18:45:57 +0200

    now: replace tld by alias

--- now/entrypoint.sh
+++ now/entrypoint.sh
@@ -3,11 +3,8 @@
 set -e
 
 REPO="$(cut -d'/' -f2 <<<"$GITHUB_REPOSITORY")"
-ALIAS="$3.$1"
+ALIAS="$1"
 DEPLOYMENT="$REPO-$2-$3"
-
-echo $ALIAS
-echo $DEPLOYMENT
 
 mv now.json now-${DEPLOYMENT}.json
 jq --arg alias "$ALIAS" --arg name "$DEPLOYMENT" '.name = $name | .alias = $alias' now-$DEPLOYMENT.json > now.json
