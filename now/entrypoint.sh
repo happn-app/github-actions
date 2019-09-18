5f8560774ec57dbc6103c81a9e27d54686fc1532
Jonathan Gautheron <gautheronjonathan@gmail.com>
2019-09-18 17:38:13 +0200

    Add now action

---
+++ now/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+REPO="$(cut -d'/' -f2 <<<"$GITHUB_REPOSITORY")"
+ALIAS="$3.$1"
+DEPLOYMENT="$REPO-$2-$3"
+
+mv now.json now-${DEPLOYMENT}.json
+jq --arg alias "$ALIAS" --arg name "$DEPLOYMENT" '.name = $name | .alias = $alias' now-$DEPLOYMENT.json > now.json
+now -t $ZEIT_TOKEN
+now -t $ZEIT_TOKEN alias $ALIAS
