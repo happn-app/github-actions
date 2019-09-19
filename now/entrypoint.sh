047dee5a075c3b783b536e599741d7081cdcfac0
Jonathan Gautheron <gautheronjonathan@gmail.com>
2019-09-19 22:31:32 +0200

    now: make prod deployment not mandatory

--- now/entrypoint.sh
+++ now/entrypoint.sh
@@ -9,5 +9,10 @@ DEPLOYMENT="$REPO-$2-$3"
 cp .env.${2}-${3} .env.production
 mv now.json now-${DEPLOYMENT}.json
 jq --arg alias "$ALIAS" --arg name "$DEPLOYMENT" '.name = $name | .alias = $alias' now-$DEPLOYMENT.json > now.json
-now -t $ZEIT_TOKEN
-now -t $ZEIT_TOKEN --prod
+
+case $4 in
+  (true)    url=$(now -t $ZEIT_TOKEN --prod);;
+  (false)   url=$(now -t $ZEIT_TOKEN);;
+esac
+
+echo ::set-output name=url::$url
