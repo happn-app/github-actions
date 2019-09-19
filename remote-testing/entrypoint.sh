bc6eb232f2bb780db8fb6ab66a3d45a76883ad8a
Jonathan Gautheron <gautheronjonathan@gmail.com>
2019-09-19 09:12:54 +0200

    Add remote-testing action, rename env to stage

---
+++ remote-testing/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+export STAGE=$1
+export REGION_ISO2=$2
+
+cp .env.$STAGE-$REGION_ISO2 .env.production
+npm ci
+npm run build
+ws --https --http2 --spa index.html --compress -d build & sleep 2
+npm run test
