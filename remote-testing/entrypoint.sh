bedb8d38ceda1bc39fefc3b48c7c2ae8534230cd
Jonathan Gautheron <gautheronjonathan@gmail.com>
2019-09-19 23:13:26 +0200

    Simplify remote-testing

--- remote-testing/entrypoint.sh
+++ remote-testing/entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 set -e
-export STAGE=$1
-export REGION_ISO2=$2
 
-cp .env.$STAGE-$REGION_ISO2 .env.production
+export CI=true
+export REACT_APP_TEST=true
+
 npm ci
-npm run build
-ws --https --http2 --spa index.html --compress -d build & sleep 2
-npm run test
+npm test
