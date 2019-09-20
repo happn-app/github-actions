d82109ad798748a8c4792c1730b0f11bfa4f2c46
Jonathan Gautheron <gautheronjonathan@gmail.com>
2019-09-20 10:06:40 +0200

    Remove action debug

--- comment/entrypoint.sh
+++ comment/entrypoint.sh
@@ -9,9 +9,8 @@ fi
 
 message=$1
 
-echo "Making a comment"
 payload=$(echo '{}' | jq --arg body "$message" '.body = $body')
 comments_url=$(cat /github/workflow/event.json | jq -r .pull_request.comments_url)
-echo $payload
-echo $comments_url
 curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$payload" "$comments_url" > /dev/null
+
+echo "Comment `$message` posted"
