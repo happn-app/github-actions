0d4a514c0a8edd4684421ff7f82cfb638dd1f652
chreble <christopheeble@gmail.com>
2019-12-12 12:01:07 +0100

    Expand topic variable

--- gcloud/pubsub/entrypoint.sh
+++ gcloud/pubsub/entrypoint.sh
@@ -7,6 +7,17 @@ topic=$2
 expiration_period=$3
 push_endpoint=$4
 
+function expand_var {
+  local var
+  # Escape ALL chars. that could trigger an expansion..
+  IFS= read -r -d '' var < <(printf %s "$1" | tr '`([$' '\1\2\3\4')
+  # ... then selectively reenable ${ references
+  var=${var//$'\4'{/\${}
+  # Finally, escape embedded double quotes to preserve them.
+  var=${var//\"/\\\"}
+  eval "printf '%s\n' \"$var\"" | tr '\1\2\3\4' '`([$'
+}
+
 # Configures Google Cloud SDK
 function setup {
   echo $GCLOUD_SERVICE_KEY | gcloud auth activate-service-account --key-file=-
@@ -28,6 +39,7 @@ function inject_runtime_config {
 
 # Adds a subscription to pub/sub service
 function add_subscription {
+    topic=$(expand_var $topic)
     subscription=$(gcloud pubsub subscriptions list --filter "name = projects/${GCLOUD_PROJECT_ID}/subscriptions/${name}" 2> /dev/null)
     if [[ $subscription == *"projects/${GCLOUD_PROJECT_ID}/${name}"* ]]; then
       # Check if subscription needs update
