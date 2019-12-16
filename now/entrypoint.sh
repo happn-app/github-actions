da493903432c9dd212fa0660a3069f72844c62f7
chreble <christopheeble@gmail.com>
2019-12-16 15:51:21 +0100

    Remove Docker instructions

--- now/entrypoint.sh
+++ now/entrypoint.sh
@@ -27,8 +27,6 @@ function setup {
   echo $GCLOUD_SERVICE_KEY | gcloud auth activate-service-account --key-file=-
   gcloud config set project ${GCLOUD_PROJECT_ID}
   gcloud config set compute/region ${GCLOUD_REGION}
-  gcloud config set run/platform managed
-  gcloud auth configure-docker -q
 }
 
 # Export runtime config variables into current bash session
