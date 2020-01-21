591b92362473ac1cb4a41c9dd989934725c277fe
chreble <christopheeble@gmail.com>
2020-01-21 11:24:44 +0100

    Refactor CloudRun

--- gcloud/cloudrun/entrypoint.sh
+++ gcloud/cloudrun/entrypoint.sh
@@ -4,21 +4,31 @@ set -e
 
 alias=$1
 is_public=$2
-flags=$3
+env_vars=$3
 log_level=$4
 log_format=$5
 add_iam_binding=$6
+max_instances=$7
+memory=$8
+concurrency=$9
+timeout=$10
+async=$11
 
 case $is_public in
   (true)    allow_unauthenticated=--allow-unauthenticated;;
   (false)   allow_unauthenticated=;;
 esac
 
+case $async in
+  (true)    async=--async;;
+  (false)   async=;;
+esac
+
 function expand_vars {
   local line lineEscaped
   while IFS= read -r line || [[ -n $line ]]; do  # the `||` clause ensures that the last line is read even if it doesn't end with \n
     # Escape ALL chars. that could trigger an expansion..
-    IFS= read -r -d '' lineEscaped < <(printf %s "$line" | tr '`([$' '\1\2\3\4')
+    IFS= read -r -d '' lineEscaped < <(printf %s "--update-env-vars $line" | tr '`([$' '\1\2\3\4')
     # ... then selectively reenable ${ references
     lineEscaped=${lineEscaped//$'\4'{/\${}
     # Finally, escape embedded double quotes to preserve them.
@@ -62,17 +72,22 @@ function build_tag_push_container {
 # Deploys function to Cloudrun
 function deploy {
   # List environment variables
-  flags=$(expand_vars <<< $flags)
+  env_vars=$(expand_vars <<< "$env_vars")
   gcloud run deploy ${alias} \
     --quiet \
+    ${async} \
     ${allow_unauthenticated} \
+    --max-instances ${max_instances} \
+    --memory ${memory} \
+    --concurrency ${concurrency} \
+    --timeout ${timeout} \
     --project ${GCLOUD_PROJECT_ID} \
     --image ${GCLOUD_CONTAINER_REGISTRY}/${GCLOUD_PROJECT_ID}/${alias} \
     --region ${GCLOUD_REGION} \
     --update-env-vars LOG_LEVEL=${log_level} \
     --update-env-vars LOG_FORMAT=${log_format} \
     --update-env-vars STAGE=${STAGE} \
-    ${flags}
+    ${env_vars}
 }
 
 # Gets the URL of the Cloudrun service
