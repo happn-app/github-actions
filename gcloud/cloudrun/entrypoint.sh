5bcd5172496129b8de1f960fed34a87026b72c97
Jonathan Gautheron <jgautheron@users.noreply.github.com>
2020-07-03 13:17:53 +0200

    Add support for labels

--- gcloud/cloudrun/entrypoint.sh
+++ gcloud/cloudrun/entrypoint.sh
@@ -87,6 +87,7 @@ function deploy {
     --project ${GCLOUD_PROJECT_ID} \
     --image ${GCLOUD_CONTAINER_REGISTRY}/${GCLOUD_PROJECT_ID}/${alias} \
     --region ${GCLOUD_REGION} \
+    --labels service=${alias} \
     --update-env-vars ALIAS=${alias} \
     --update-env-vars LOG_LEVEL=${log_level} \
     --update-env-vars LOG_FORMAT=${log_format} \
