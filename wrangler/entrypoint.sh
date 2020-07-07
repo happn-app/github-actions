a96180ada55f79f2190de857313f94eb6a7ea0fd
Jonathan Gautheron <gautheronjonathan@gmail.com>
2020-07-07 21:03:30 +0200

    dotenv: add new action

---
+++ wrangler/entrypoint.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+set -e
+
+api_token=$1
+vars=$2
+use_runtime_config=$3
+
+export CF_API_TOKEN="$api_token"
+
+function expand_vars {
+  local line lineEscaped
+  while IFS= read -r line || [[ -n $line ]]; do  # the `||` clause ensures that the last line is read even if it doesn't end with \n
+    # Escape ALL chars. that could trigger an expansion..
+    IFS= read -r -d '' lineEscaped < <(printf %s "$line" | tr '`([$' '\1\2\3\4')
+    # ... then selectively reenable ${ references
+    lineEscaped=${lineEscaped//$'\4'{/\${}
+    # Finally, escape embedded double quotes to preserve them.
+    lineEscaped=${lineEscaped//\"/\\\"}
+    eval "printf '%s\n' \"$lineEscaped\"" | tr '\1\2\3\4' '`([$'
+  done
+}
+
+# Configures Google Cloud SDK
+function setup {
+  echo $GCLOUD_SERVICE_KEY | gcloud auth activate-service-account --key-file=-
+  gcloud config set project ${GCLOUD_PROJECT_ID}
+  gcloud config set compute/region ${GCLOUD_REGION}
+
+  echo $GCLOUD_SERVICE_KEY | cat > /tmp/account.json
+  export GOOGLE_APPLICATION_CREDENTIALS=/tmp/account.json
+}
+
+# Export runtime config variables into current bash session
+function inject_runtime_config {
+  export $(
+    gcloud beta runtime-config configs variables list \
+      --values \
+      --config-name ${STAGE}-${REGION} \
+      --format='json' \
+      | jq -r '.[] | [(.name | split("/") | join("_") | split("-") | join("_") | ascii_upcase), .value] | join("=")' \
+      | xargs
+  )
+}
+
+# Writes environment file expanding variables not yet expanded such as Berglas vars
+function write_env_file {
+  cat /dev/null > .env.production
+  IFS=$'\n'
+  for item in $1
+  do
+    if [[ "$item" == *berglas* ]] ;
+    then
+      var=$(echo "$item" | sed 's/^\([A-Z0-9_]*\)=\(.*\)/\2/' | xargs)
+      var_expanded=$(berglas access $var)
+      echo "$item" | sed "s~${var}~$var_expanded~g" >> .env.production
+    else
+      echo "$item" >> .env.production
+    fi
+  done
+}
+
+function deploy {
+  wrangler publish --env ${STAGE}
+}
+
+setup
+if [[ ! -z "${vars}" ]]; then
+  case $use_runtime_config in
+    (true) inject_runtime_config;;
+  esac
+fi
+if [[ ! -z "${vars}" ]]; then
+  parsed=$(expand_vars <<< "${vars}")
+  write_env_file "$parsed"
+fi
+deploy
