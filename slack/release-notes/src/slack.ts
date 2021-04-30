784b8ce6fa4d6dbef1ec342757cc6d98a491dd61
Piotr Kubisa <piotrkubisa@users.noreply.github.com>
2021-04-30 16:41:20 +0200

    ✨slack/release-notes: Publish changelog in a thread, when "message" is configured (#11)
    * ✨ Split changelog into 8k-chars-long messages
    * 🩹 Make sure chunks are below limit after joining
    * ✏️ Drop superfluous bracket in the release link
    * ♻️ Try to refrain from usage of any type
    * 📦 Add compiled slack/release-notes action
    * 🐛 Post messages in thread when message is provided
    * 💄 Leave up to user to use slack markdown

---
+++ slack/release-notes/src/slack.ts
@@ -0,0 +1,130 @@
+import { Block, ChatPostMessageArguments, KnownBlock, WebClient } from '@slack/web-api'
+
+// @ts-ignore
+const slack = new WebClient(process.env.SLACK_TOKEN)
+
+type StartThreadParams = {
+  message: string
+  releaseName: string
+  releaseURL: string,
+  repo: string
+  repositoryURL: string
+  tagName: string
+
+  channel: string
+  chatArgs: { [K in keyof ChatPostMessageArguments]+?: ChatPostMessageArguments[K] }
+}
+
+export async function startThread(
+  {
+    message,
+    releaseName,
+    releaseURL,
+    repo,
+    repositoryURL,
+    tagName,
+    channel,
+    chatArgs,
+  }: StartThreadParams,
+) {
+  const text = message
+    .replace(/{{.?tag.?}}/, tagName)
+    .replace(/{{.?release.?}}/, releaseName || tagName)
+
+  let params: ChatPostMessageArguments = {
+    channel: channel,
+    mrkdwn: true,
+    text,
+    blocks: [
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text,
+        },
+      },
+      {
+        type: 'context',
+        elements: [
+          {
+            type: 'mrkdwn',
+            text: `<${releaseURL}|${tagName}>`,
+          },
+          {
+            type: 'mrkdwn',
+            text: `<${repositoryURL}|${repo}>`,
+          },
+        ],
+      },
+    ],
+    ...chatArgs,
+  }
+  return await slack.chat.postMessage(params)
+}
+
+type PostChangelogParams = {
+  baseTag: string | undefined
+  changelog: string
+  diffURL: string | undefined
+  releaseName: string
+  releaseURL: string
+  tag: string
+
+  channel: string
+  chatArgs: { [K in keyof ChatPostMessageArguments]+?: ChatPostMessageArguments[K] }
+}
+
+export async function postChangelog(
+  {
+    releaseName,
+    releaseURL,
+    changelog,
+    diffURL,
+    baseTag,
+    tag,
+    channel,
+    chatArgs,
+  }: PostChangelogParams,
+) {
+  let blocks: (Block | KnownBlock)[] = [
+    {
+      type: 'header',
+      text: {
+        type: 'plain_text',
+        text: `Changelog of ${releaseName || tag}`,
+        emoji: true,
+      },
+    },
+    {
+      type: 'context',
+      elements: [
+        {
+          type: 'mrkdwn',
+          text: changelog,
+        },
+      ],
+    },
+    {
+      type: 'divider',
+    },
+    {
+      type: 'context',
+      elements: [
+        {
+          type: 'mrkdwn',
+          text: diffURL && baseTag
+            ? `See also <${releaseURL}|full release notes on GitHub> or a <${diffURL}|diff between ${baseTag} and ${tag}>.`
+            : `See also <${releaseURL}|full release notes on GitHub>.`,
+        },
+      ],
+    },
+  ]
+
+  let params: ChatPostMessageArguments = {
+    channel,
+    text: `Changelog of ${releaseName}`,
+    blocks: blocks,
+    ...chatArgs,
+  }
+  return await slack.chat.postMessage(params)
+}
