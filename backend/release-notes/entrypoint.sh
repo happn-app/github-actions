faf7b4f76f5efe4330bae1b228a8438c0981ff86
croquette <christophe.roquette.pro@gmail.com>
2021-12-10 17:17:33 +0100

    Add JIRA & PR format for slack & MD

--- backend/release-notes/entrypoint.sh
+++ backend/release-notes/entrypoint.sh
@@ -2,6 +2,29 @@
 
 set -e
 
+format_github_output() {
+    OUTPUT_CHANGELOG=$1
+    OUTPUT_CHANGELOG="${OUTPUT_CHANGELOG//'%'/'%25'}"
+    OUTPUT_CHANGELOG="${OUTPUT_CHANGELOG//$'\n'/'%0A'}"
+    OUTPUT_CHANGELOG="${OUTPUT_CHANGELOG//$'\r'/'%0D'}"
+    echo "$OUTPUT_CHANGELOG"
+}
+
+format_for_markdown() {
+  MESSAGE=$CHANGELOG
+  FORMATTED_JIRA=$(echo "$MESSAGE" | sed -E 's|([A-Z]+-[0-9]+)|[\1](https://happnapp.atlassian.net/browse/\1)|g')
+  
+  echo ""$FORMATTED_JIRA""
+}
+format_for_slack() {
+  MESSAGE=$CHANGELOG
+  FORMATTED_JIRA=$(echo "$MESSAGE" | sed -E 's!([A-Z]+-[0-9]+)!<https://happnapp.atlassian.net/browse/\1|\1>!g')
+  FORMATTED_PR=$(echo "$FORMATTED_JIRA" | sed -E "s!#([0-9]+)!<$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/\1|#\1>!g")
+  
+  echo "$FORMATTED_PR"
+}
+
+
 echo "Resolving tags"
 git fetch --tags > /dev/null 2>&1
 
@@ -23,8 +46,13 @@ NB_COMMITS=$(echo -n "$CHANGELOG" | wc -l)
 echo "Found $NB_COMMITS commits"
 echo "$CHANGELOG"
 
-OUTPUT_CHANGELOG=$CHANGELOG
-OUTPUT_CHANGELOG="${OUTPUT_CHANGELOG//'%'/'%25'}"
-OUTPUT_CHANGELOG="${OUTPUT_CHANGELOG//$'\n'/'%0A'}"
-OUTPUT_CHANGELOG="${OUTPUT_CHANGELOG//$'\r'/'%0D'}"
+SLACK_OUTPUT=$(format_for_slack "$CHANGELOG")
+MD_OUTPUT=$(format_for_markdown "$CHANGELOG")
+
+OUTPUT_CHANGELOG=$(format_github_output "$CHANGELOG")
+OUTPUT_CHANGELOG_SLACK=$(format_github_output "$SLACK_OUTPUT")
+OUTPUT_CHANGELOG_MD=$(format_github_output "$MD_OUTPUT")
+
 echo "::set-output name=changelog::$OUTPUT_CHANGELOG"
+echo "::set-output name=changelog_slack::$OUTPUT_CHANGELOG_SLACK"
+echo "::set-output name=changelog_md::$OUTPUT_CHANGELOG_MD"
