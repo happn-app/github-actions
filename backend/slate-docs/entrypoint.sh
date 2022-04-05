83f4cf96bd36f85ffbfe25f600db480546589e7d
croquette <christophe.roquette.pro@gmail.com>
2022-04-05 18:40:50 +0200

    Slate-docs builder action

--- backend/slate-docs/entrypoint.sh
+++ backend/slate-docs/entrypoint.sh
@@ -5,10 +5,11 @@ set -o pipefail
 
 main() {
    cp -R $DOC_BASE_FOLDER/* /srv/slate/source
+   mkdir -p build
    CUR_DIR=$(pwd)
    cd /srv/slate
    /srv/slate/slate.sh build
-   mv /srv/slate/build $CUR_DIR/build
+   cp -R /srv/slate/build/* $CUR_DIR/build/
 }
 
 main "$@"
